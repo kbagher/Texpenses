c1d6fcc0fde458641d1ce3f9b39ae6a9badf9878
Kassem Bagher <kassem@bagher.me>
2017-08-25 09:22:50 +1000

    handled keyboard push

--- Texpenses/Texpenses/DashboardViewController.swift
+++ Texpenses/Texpenses/DashboardViewController.swift
@@ -8,25 +8,84 @@
 
 import UIKit
 
-class DashboardViewController: UIViewController {
+class DashboardViewController: UIViewController,UITextFieldDelegate {
 
     @IBOutlet weak var currency: UIView?
     @IBOutlet weak var expenses: UIView?
     @IBOutlet weak var exchangeRate: UIView?
+    @IBOutlet weak var currecnySymbol: UILabel!
+    @IBOutlet weak var currecnyName: UILabel!
+    @IBOutlet weak var expensesSoFar: UILabel!
+    @IBOutlet weak var rate: UITextField!
+    @IBOutlet weak var averageExchangeRate: UILabel!
     
+    
+    
+    // MARK: - View lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         setStyleFor(view: currency!)
         setStyleFor(view: exchangeRate!)
         setStyleFor(view: expenses!)
+       
     }
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        registerNotifications()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        unregisterNotifications()
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
+
+    // MARK: - Keyboard
+    // MARK: Keyboard visibility
+    
+    func registerNotifications() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: .UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: .UIKeyboardWillHide, object: nil)
+    }
+    
+    func unregisterNotifications() {
+        NotificationCenter.default.removeObserver(self, name: .UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.removeObserver(self, name: .UIKeyboardWillHide, object: nil)
+    }
+    
+    func keyboardWillShow(notification: NSNotification){
+        let keyboardInfo  = notification.userInfo as NSDictionary?
+        let keyboardFrameEnd: NSValue? = (keyboardInfo?.value(forKey: UIKeyboardFrameEndUserInfoKey) as? NSValue)
+        let keyboardFrameEndRect: CGRect? = keyboardFrameEnd?.cgRectValue
+        print( exchangeRate!.frame.origin.y + rate.frame.origin.y )
+        
+        if exchangeRate!.frame.origin.y + rate.frame.origin.y + rate.frame.size.height + 10 > (keyboardFrameEndRect?.origin.y)! {
+            print("iff")
+            self.view.frame.origin.y = -(self.exchangeRate!.frame.origin.y + self.rate.frame.origin.y + self.rate.frame.size.height - (keyboardFrameEndRect?.origin.y)!) - 30.0
+        }
+    }
+    
+    func keyboardWillHide(notification: NSNotification){
+        print("will hide")
+        self.view.frame.origin.y = 0
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        rate.resignFirstResponder()
+    }
+    
+    // MARK: input validation and formatting
+    
+    
+    
+    // MARK: - UI
     func setStyleFor(view v:UIView) {
         // Cell desing (border and background color
         v.layer.masksToBounds = true
@@ -35,7 +94,7 @@ class DashboardViewController: UIViewController {
         v.layer.backgroundColor = UIColor(red:0.96, green:0.96, blue:0.96, alpha:1.0).cgColor
         v.layer.borderColor = UIColor.lightGray.cgColor
     }
-
+    
     /*
     // MARK: - Navigation
 
