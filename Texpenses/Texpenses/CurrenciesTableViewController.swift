eadfb4acb58bcd1858185ba3e553f92fd362abff
Kassem Bagher <kassem@bagher.me>
2017-10-03 12:40:02 +1100

    commented CurrenciesTableViewController

--- Texpenses/Texpenses/CurrenciesTableViewController.swift
+++ Texpenses/Texpenses/CurrenciesTableViewController.swift
@@ -10,69 +10,85 @@ import UIKit
 
 class CurrenciesTableViewController: UITableViewController,WebServicesDelegate {
 
+    // MARK: - Class Variables
     let model = Model.sharedInstance
     var currencies = Model.sharedInstance.getCurrencies()
     let web = WebServices.sharedInstance
-    
     var oldSelectedCell = 0
     
+    
+    // MARK: - View lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
         web.delegate = self
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        
+        // remove extra empty cells
         tableView.tableFooterView = UIView()
     }
     override func viewWillAppear(_ animated: Bool) {
+        // Load currencies
         if currencies == nil{
+            // from server
             LoadingView.showIndicator("Fetching Currencies From Server")
             web.getCurrencies()
         }
         else{
+            // from local database
             loadCurrencies()
         }
     }
 
     override func viewWillDisappear(_ animated: Bool) {
+        // release all delegates
         web.delegate = nil
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
     // MARK: - Helping methods
     
+    /// Reload table contents in main thread
     func refreshTable(){
-        tableView.reloadData()
-    }
-    
-    func loadCurrencies(){
-        currencies = model.getCurrencies()
         DispatchQueue.main.async(execute: {
-            self.refreshTable()
+            self.tableView.reloadData()
         })
     }
     
+    /// Load currencies from local database
+    func loadCurrencies(){
+        currencies = model.getCurrencies()
+        refreshTable()
+    }
+    
+    /// Hide activity view in main thread
     func hideActivityView(){
         DispatchQueue.main.async {
             LoadingView.hideIndicator()
         }
     }
 
-    // MARK: Delegates
+    // MARK: - Delegates
+    
+    /// Handles retreiving currencies delegate
+    ///
+    /// # See Also:
+    /// WebServiceDelegate -> didRetrieveCurrencies()
     func didRetrieveCurrencies(numOfCurrencies: Int){
         print("got currencies")
         hideActivityView()
         loadCurrencies()
     }
+    
+    /// Handles error while retreiving currencies delegate
+    ///
+    /// # See Also:
+    /// WebServiceDelegate -> didRetrieveCurrenciesError()
     func didRetrieveCurrenciesError(error: NSError){
+        
         hideActivityView()
+        
         let alert = UIAlertController(title: "Error", message: "Cannot retrieve currencies from the server 🙁", preferredStyle: UIAlertControllerStyle.alert);
         alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.cancel, handler: nil))
         present(alert, animated: true, completion: nil)
@@ -82,46 +98,59 @@ class CurrenciesTableViewController: UITableViewController,WebServicesDelegate {
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
         return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
         if let count = currencies?.count{
             return count
         }
-        return 0
+        return 0 // no currencies available
     }
 
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        
         let cell = tableView.dequeueReusableCell(withIdentifier: "currencyCell", for: indexPath)
 
+        // Grap reference to UI labels
         let currency:UILabel = cell.viewWithTag(100) as! UILabel
         let symbol = currencies?[indexPath.item].symbol
         let name = currencies?[indexPath.item].name
         
+        // Format currency label text
         currency.text = "\(name!) (\(symbol!))"
 
+        // Handle user selected base currency checkmark
         if currencies?[indexPath.item] == model.getPreferences()?.userCurrency {
+            // the base currency
             cell.accessoryType = UITableViewCellAccessoryType.checkmark
             oldSelectedCell = indexPath.item
         }
         else{
+            // other curencies
             cell.accessoryType = UITableViewCellAccessoryType.none
         }
-
         return cell
     }
  
 
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        // remove old checkmark
         tableView.cellForRow(at: IndexPath(row: oldSelectedCell, section: 0))?.accessoryType=UITableViewCellAccessoryType.none
+        
         oldSelectedCell = indexPath.item
+        
+        // update base currency in the database
         model.updatePreferenceWith(BaseCurrency: (currencies?[indexPath.item])!)
+        
+        // set checkmark on new selected currency
         tableView.cellForRow(at: indexPath)?.accessoryType=UITableViewCellAccessoryType.checkmark
         tableView.deselectRow(at: indexPath, animated: true)
+        
+        // navigating back to previous view
         navigationController?.popToRootViewController(animated: true)
     }
 
