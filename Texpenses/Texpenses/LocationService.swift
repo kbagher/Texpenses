5619a4bdbe7cd45f3f35ee37433f3d78994639a1
Kassem Bagher <kassem@bagher.me>
2017-10-04 10:53:12 +1100

    documented LocationServiceDelegate WebServicesDelegate

--- Texpenses/Texpenses/LocationService.swift
+++ Texpenses/Texpenses/LocationService.swift
@@ -9,15 +9,35 @@
 import Foundation
 import CoreLocation
 
+/// Location Service delegates
 @objc protocol LocationServiceDelegate {
+    
+    /// Location changed delegate
+    ///
+    /// - Parameter currentLocation: updated location
     func didUpdateLocation(currentLocation: CLLocation)
+    
+    /// Error while getting location
+    ///
+    /// - Parameter error: error object
     func didUpdateLocationFailWithError(error: NSError)
+    
+    /// Location Service Authorization Changed
+    ///
+    /// Called whenever the authorization of the location service
+    /// is changed.
+    /// - Parameter status: new authirization
     @objc optional func didChangeAuthorization(status: CLAuthorizationStatus)
+    
+    /// Location geocode reversed
+    ///
+    /// - Parameter place: geocode placemark
     @objc optional func didReverseGeocode(place:CLPlacemark)
 }
 
 class LocationService: NSObject, CLLocationManagerDelegate {
-    
+
+    // MARK: - Class Variables
     static let sharedInstance = LocationService()
     private var locationManager: CLLocationManager?
     var currentLocation: CLLocation?
@@ -28,13 +48,17 @@ class LocationService: NSObject, CLLocationManagerDelegate {
     override init() {
         super.init()
         
+        // initialize location manager once
         self.locationManager = CLLocationManager()
         
+        
         guard let locationManager = self.locationManager else {
-            return
+            return // location manager not initialised
         }
+        // Location required accurase and distance
         locationManager.distanceFilter = 150
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
+
         locationManager.delegate = self
     }
     
@@ -43,10 +67,12 @@ class LocationService: NSObject, CLLocationManagerDelegate {
         guard let delegate = self.delegate else {
             return
         }
-        
+        // inform the delegate that authorization has changed
         delegate.didChangeAuthorization!(status: status)
     }
     
+    
+    /// Request location service authorization from the user
     func requestAuthorization(){
         guard let locationManager = self.locationManager else {
             return
@@ -54,26 +80,32 @@ class LocationService: NSObject, CLLocationManagerDelegate {
         locationManager.requestWhenInUseAuthorization()
     }
     
+    /// Check if the app has authorization to use location service
+    ///
+    /// - Returns: true if app can use location service
     func isLocationServiceAuthorised() -> Bool{
         return CLLocationManager.authorizationStatus() == .authorizedAlways || CLLocationManager.authorizationStatus() == .authorizedWhenInUse
     }
     
+    
+    /// Start tracking user's location
     func startUpdatingLocation() {
         print("called")
         self.locationManager?.startUpdatingLocation()
     }
     
+    /// Stop tracking user's location
     func stopUpdatingLocation() {
         self.locationManager?.stopUpdatingLocation()
     }
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        print("---- updated")
+
         guard let location = locations.last else {
             return
         }
         
-        // singleton for get last(current) location
+        // update current location internally
         self.currentLocation = location
 
         guard let delegate = self.delegate else {
@@ -81,24 +113,32 @@ class LocationService: NSObject, CLLocationManagerDelegate {
             return
         }
         
+        // inform the delegate that location has changed
         delegate.didUpdateLocation(currentLocation: location)
         
     }
     
     func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
-        updateLocationDidFailWithError(error: error as NSError)
         
+        updateLocationDidFailWithError(error: error as NSError)
     }
     
+    /// Failed to get user's location
+    ///
+    /// - Parameter error: error object
     private func updateLocationDidFailWithError(error: NSError) {
         
         guard let delegate = self.delegate else {
             return
         }
-        
+        // inform the delegate that an error occured while gettings user's location
         delegate.didUpdateLocationFailWithError(error: error)
     }
     
+    
+    /// Reverse user's location
+    ///
+    /// - Parameter locaiton: Location to be reversed
     func getReversedGeocodeWith(locaiton:CLLocation){
         
         locationGeocoder.reverseGeocodeLocation(locaiton, completionHandler:
@@ -115,8 +155,10 @@ class LocationService: NSObject, CLLocationManagerDelegate {
                 let pm = placemarks! as [CLPlacemark]
                 
                 if pm.count > 0 {
+                    // has placemark
                     let pm = placemarks![0]
-                    print("reversed")
+                    
+                    // Send reversed geocode location's placemark
                     delegate.didReverseGeocode!(place: pm)
                 }}
         )
--- Texpenses/Texpenses/WebServices.swift
+++ Texpenses/Texpenses/WebServices.swift
@@ -8,29 +8,64 @@
 
 import Foundation
 
+/// Web Service delegates
 @objc protocol WebServicesDelegate {
+    
+    /// Exchange rate retrieved successfully
+    ///
+    /// Called when the exhcnage rate is retrieved successfully from the server
+    /// - Parameter rate: exchange rate
     @objc optional func didRetrieveExchangeRate(rate: Double)
+    /// Exchange rate is not retrieved
+    ///
+    /// Called when the the is an error while
+    /// retrieving the exchange rate from the server
+    /// - Parameter error: error object
     @objc optional func didRetrieveExchangeRateError(error: NSError)
+    
+    /// Currencies retrieved successfully
+    ///
+    /// Called when currencies are retrieved successfully from the server
+    /// - Parameter numOfCurrencies: number of retrieved currencies
     @objc optional func didRetrieveCurrencies(numOfCurrencies: Int)
+    /// Currencies are not retrieved
+    ///
+    /// Called when the the is an error while
+    /// retrieving currencies from the server
+    /// - Parameter error: error object
     @objc optional func didRetrieveCurrenciesError(error: NSError)
 }
 
 
 class WebServices {
     
+    // MARK: - Class Variables
     static let sharedInstance = WebServices()
     var delegate: WebServicesDelegate?
     private init(){}
-    
     let session = URLSession.shared
     
+    
+    /// Gets exchange rate from server
+    ///
+    /// This will return the exhcnage rate from the server baseed on the
+    /// user's base currencies  and the country's currency
+    /// - Parameters:
+    ///   - bc: user's base currency
+    ///   - toCurrency: country's currency
     func exchangeRateWith(BaseCurrency bc:Currency, toCurrency:Currency) {
+        
+        // create new requets with REST API URL
         var request = URLRequest(url: URL(string: "https://currencyconverter.p.mashape.com/?from=\(toCurrency.symbol!)&from_amount=1&to=\(bc.symbol!)")!)
+        // API Key header
         request.setValue("At6ickh4Aamshmzv7yKtKX8lpNUrp1JONpdjsnIEwcuCI0WbHT", forHTTPHeaderField: "X-Mashape-Key")
+        // tell the API to return json only
         request.setValue("application/json", forHTTPHeaderField: "Accept")
         
+        // URL request task
         let task = session.dataTask(with: request, completionHandler: {data, response, downloadError in
-            // Handler in the case of an error
+            
+            // Request error
             if let error = downloadError
             {
                 print("\(String(describing: data)) \n data")
@@ -40,12 +75,11 @@ class WebServices {
             }
             else
             {
-                // Create a variable to hold the results once they have been passed through the JSONSerialiser.
-                // Why has this variable been declared with an explicit data type of Any
+                // holds the request results
                 let parsedResult: Any!
                 do
                 {
-                    // Convert the http response payload to JSON.
+                    // Convert results to JSON object
                     parsedResult = try JSONSerialization.jsonObject(with: data!, options: JSONSerialization.ReadingOptions.allowFragments)
                 }
                 catch let error as NSError
@@ -54,23 +88,34 @@ class WebServices {
                     self.delegate?.didRetrieveExchangeRateError!(error: error)
                 }
                 if let results = parsedResult as? NSDictionary {
-                    var rate = Double(results.value(forKey: "to_amount") as! Double)
-//                    rate = Double(round(100 * rate)/100)
-                    rate = Model.sharedInstance.formatDecimalPoints(Number: rate)
-                    self.delegate?.didRetrieveExchangeRate!(rate: rate)
+                    // Extract ecchange rate from JSON
+                    if let  stringRate = results.value(forKey: "to_amount"){
+                        var rate = Double(stringRate as! Double)
+                        rate = Model.sharedInstance.formatDecimalPoints(Number: rate)
+                        self.delegate?.didRetrieveExchangeRate!(rate: rate)
+                    }
+                    else{
+                        // wrong json data, sent by server
+                        self.delegate?.didRetrieveExchangeRateError!(error: NSError())
+                    }
                 }
-                
             }
         })
+        
+        // run the task
         task.resume()
     }
     
     
+    /// Gets currencies from the server
     func getCurrencies(){
+        // create new requets with REST API URL
         let request = URLRequest(url: URL(string: "http://currencyconverter.kund.nu/api/availablecurrencies/")!)
+        
+        // URL request task
         let task = session.dataTask(with: request, completionHandler: {data, response, downloadError in
-            print("HERE")
-            // Handler in the case of an error
+            
+            // Request error
             if let error = downloadError
             {
                 print("\(String(describing: data)) \n data")
@@ -80,12 +125,11 @@ class WebServices {
             }
             else
             {
-                // Create a variable to hold the results once they have been passed through the JSONSerialiser.
-                // Why has this variable been declared with an explicit data type of Any
+                // holds the request results
                 let parsedResult: Any!
                 do
                 {
-                    // Convert the http response payload to JSON.
+                    // Convert response to JSON object
                     parsedResult = try JSONSerialization.jsonObject(with: data!, options: JSONSerialization.ReadingOptions.allowFragments)
                 }
                 catch let error as NSError
@@ -95,14 +139,18 @@ class WebServices {
                 }
                 
                 if let results = parsedResult as? NSArray {
+                    // Extract currencies from JSON Dictionary
                     for item in results{
+                        // extract currency information
                         let cur = item as! NSDictionary
                         let symbol = String(describing: cur.value(forKey: "id")!)
                         var name:String = String(describing: cur.value(forKey: "description")!)
                         name = name.substring(from: (name.index(name.startIndex, offsetBy: 4)))
                         name = name.replacingOccurrences(of: ",", with: "")
                         name = name.replacingOccurrences(of: "  ", with: " ")
-                        let _ = Model.sharedInstance.addCurrency(name: name, symbol: symbol)
+                        
+                        // Add currency to the database
+                        Model.sharedInstance.addCurrency(name: name, symbol: symbol)
                     }
                     self.delegate?.didRetrieveCurrencies!(numOfCurrencies: results.count)
                 }
